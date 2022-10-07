94d5c03c3bf08e1aa6e0184a31a829201882de95
JOSÉ ALFREDO ZARAGOZA MARCO <joszarmar@alu.edu.gva.es>
2022-10-07 11:41:17 +0200

    Cambios relevantes en el código

--- Tema3-VisorImagenes/MainWindow.xaml.cs
+++ Tema3-VisorImagenes/MainWindow.xaml.cs
@@ -20,9 +20,72 @@ namespace Tema3_VisorImagenes
     /// </summary>
     public partial class MainWindow : Window
     {
+        private int numeroFotos;
         public MainWindow()
         {
             InitializeComponent();
+            CambiarTexto();
+        }
+
+        private void añadirImagenButton_Click(object sender, RoutedEventArgs e)
+        {
+            Image imagen = new Image();
+            imagen.Height = 120;
+
+            BitmapImage logo = new BitmapImage();
+            logo.BeginInit();
+            logo.UriSource = new Uri("./assets/episodioIV.jpg", UriKind.Relative);
+            logo.EndInit();
+
+            imagen.Source = logo;
+
+            panelImagenesWrapPanel.Children.Add(imagen);
+
+            CambiarTexto();
+        }
+
+        private void eliminarImagen_Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (panelImagenesWrapPanel.Children.Count > 0)
+            {
+                panelImagenesWrapPanel.Children.RemoveAt(panelImagenesWrapPanel.Children.Count - 1);
+
+                CambiarTexto();
+            }
+            
+        }
+
+        private void eliminarTodas_Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (panelImagenesWrapPanel.Children.Count > 0)
+            {
+                panelImagenesWrapPanel.Children.Clear();
+
+                CambiarTexto();
+            }
+        }
+
+        private void rotar_Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            eliminarImagen_Button_Click(sender, e);
+            BitmapImage bitmap = new BitmapImage();
+
+            Image image = new Image();
+            image.Height = 120;
+            bitmap.BeginInit();
+            bitmap.UriSource = new Uri("./assets/episodioIV.jpg", UriKind.Relative);
+            bitmap.Rotation = Rotation.Rotate90;
+            bitmap.EndInit();
+
+            image.Source = bitmap;
+            panelImagenesWrapPanel.Children.Add(image);
+        }
+
+        private void CambiarTexto()
+        {
+            numeroFotos = panelImagenesWrapPanel.Children.Count;
+            numeroImagenes_TextBlock.Text = "Nº de imagenes: " + numeroFotos;
         }
     }
 }
